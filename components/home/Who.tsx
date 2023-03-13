07950dcfa818355a1dd412dbc14e7b7e094c9d25
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-03-13 13:53:51 -0300

    chore: update og image seo + add Who cards description

--- components/home/Who.tsx
+++ components/home/Who.tsx
@@ -2,6 +2,30 @@ import H2 from "@/components/ui/H2"
 import CardIcon from "@/components/home/ui/CardIcon"
 import Description from "@/components/ui/Description"
 
+interface Card {
+	title: string
+	body: string
+	image: string
+}
+
+const cards: Card[] = [
+	{
+		title: "TUVE VARICELA",
+		body: "Después de la varicela, el Virus Varicela Zóster permanece latente en el sistema nervioso y puede reactivarse con la edad.",
+		image: "varicela",
+	},
+	{
+		title: "TENGO 50 AÑOS O MÁS",
+		body: "A medida que las personas envejecen, las células del sistema inmune se debilitan, por lo que aumenta el riesgo de desarrollar la enfermedad. Se calcula que aproximadamente más del 90%",
+		image: "mayor",
+	},
+	{
+		title: "MI SISTEMA INMUNE ESTÁ COMPROMETIDO",
+		body: "La incidencia del Herpes Zóster en poblaciones inmunocomprometidas crece exponencialmente. Por ejemplo, en personas que hayan recibido trasplante de médula ósea o células madre, la probabilidad de incidencia respecto a la población total es 9 veces mayor.",
+		image: "inmune",
+	},
+]
+
 const Who = () => {
 	return (
 		<section className="text-center space-y-6">
@@ -10,10 +34,10 @@ const Who = () => {
 				<Description title={`Enterate si estás en riesgo de contraer Herpes Zóster.`}/>
 			</div>
 
-			<div className="grid grid-cols-1 md:grid-cols-3 content-start gap-x-10 gap-y-8 lg:gap-y-0 px-12">
-				<CardIcon title={`TUVE VARICELA`} image={`varicela`}/>
-				<CardIcon title={`TENGO 50 AÑOS O MÁS `} image={`mayor`}/>
-				<CardIcon title={`MI SISTEMA INMUNE ESTÁ COMPROMETIDO`} image={`inmune`}/>
+			<div className="grid grid-cols-1 lg:grid-cols-3 content-start gap-x-0 lg:gap-x-10 gap-y-8 lg:gap-y-0 px-8 md:px-20 lg:px-12">
+				{cards.map(({title, body, image}: Card, index: number) => (
+					<CardIcon key={index} title={title} body={body} image={image}/>
+				))}
 			</div>
 		</section>
 	)
--- components/home/ui/CardIcon.tsx
+++ components/home/ui/CardIcon.tsx
@@ -4,14 +4,16 @@ import H3 from "@/components/ui/H3"
 
 interface CardIcon {
 	title: string
+	body: string
 	image: string
 }
 
-const CardIcon = ({title, image}: CardIcon) => {
+const CardIcon = ({title, image, body}: CardIcon) => {
 	return (
-		<article className="flex flex-col w-full gap-y-6 items-center bg-gsk-gray rounded-xl shadow-md px-10 py-8 text-gsk-orange">
+		<article className="flex flex-col w-full gap-y-6 items-center bg-white border border-gsk-gray rounded-xl shadow-md px-10 md:px-6 xl:px-10 py-8 text-gsk-orange">
 			<Image src={`/assets/images/${image}.svg`} alt={`Ícono Varicela`} width={80} height={80}/>
 			<H3 title={title}/>
+			<p className="text-black text-lg">{body}</p>
 		</article>
 	)
 }
