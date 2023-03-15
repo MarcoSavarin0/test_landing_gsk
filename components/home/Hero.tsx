01669e4a3f49732283a163e5bae521c27d7c1951
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-03-15 18:19:16 -0300

    chore: update vimeo tag props

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -10,7 +10,8 @@ const Hero = () => {
 	return (
 		<section>
 			<div className="relative w-full flex flex-col h-auto md:h-[27rem] lg:h-[34rem] xl:h-[40rem] 2xl:h-[44rem] bg-gsk-dark overflow-hidden">
-				<video className="order-last md:order-first block w-full h-auto object-cover object-center aspect-video -mt-10 md:mt-0 lg:-mt-8 xl:-mt-20 2xl:-mt-40 " src="/assets/videos/hero.mp4" poster="/assets/images/hero.jpg" width="1280" height="700" autoPlay={true} controls={false} loop={true}>
+				<video className="order-last md:order-first block w-full h-auto object-cover object-center aspect-video -mt-10 md:mt-0 lg:-mt-8 xl:-mt-20 2xl:-mt-40 " preload="auto" muted={true} width="1280" height="700" autoPlay={true} controls={false} loop={true}>
+					<source src="/assets/videos/hero.mp4" type="video/mp4"/>
 					<Image priority src={`/assets/images/hero.jpg`} alt={`Herpes Zóster`} width="1440" height="747" className="block w-full h-full object-cover object-center"/>
 				</video>
 
