354d77c0ec27c02b1c6224c0a3ecbedb9414d488
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-03-16 15:06:17 -0300

    fix: description component props + delete modal inside acerca-de page

--- pages/acerca-de-herpes-zoster.tsx
+++ pages/acerca-de-herpes-zoster.tsx
@@ -1,6 +1,5 @@
 import { NextSeo } from "next-seo"
 
-import Modal from '@/components/ui/Modal'
 import Blog from "@/components/home/Blog"
 import Banner from "@/components/home/Banner"
 import H2 from "@/components/ui/H2"
@@ -53,11 +52,6 @@ const AcercaDe = () => {
 				<Blog/>
 				<Banner/>
 			</>
-
-			<Modal>
-				<H2 title={`¿QUÉ ES EL HERPES ZÓSTER?`}/>
-				<Description title={`El Herpes Zóster, también llamado coloquialmente culebrilla, es una enfermedad causada por la reactivación del Virus Varicela Zóster (VVZ), el mismo virus que causa la varicela. Luego de que una persona contrae varicela <sup>1 2</sup>, el virus permanece latente en su sistema nervioso, por lo general, sin causar síntomas hasta su reactivación.`}/>
-			</Modal>
 		</>
 	)
 }
