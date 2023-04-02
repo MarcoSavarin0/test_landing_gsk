da60f65506a071f08f090affa7ed7a6567547f34
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-04-02 01:58:58 -0300

    chore: update blog label cta at home + update text at banner comp + reorder footer menu items

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -2,11 +2,14 @@ import Image from "next/image"
 
 const Banner = () => {
 	return (
-		<section className="relative bg-gsk-orange text-center py-14">
+		<section className="relative bg-gsk-orange text-white text-center py-14">
 			<div className="absolute top-0 flex w-full justify-center">
 				<Image src="/shape-gsk-white.svg" width={80} height={60} alt="" />
 			</div>
-			<h2 className="text-white text-xl md:text-3xl leading-relaxed uppercase font-bold px-6">CONSULTÁ CON TU MÉDICO O MÉDICA ACERCA DE LA PREVENCIÓN DEL HÉRPES ZÓSTER</h2>
+			<div className="space-y-2 px-6">
+				<h2 className="text-xl md:text-3xl leading-relaxed uppercase font-bold">Activemos antes de que el virus se reactive</h2>
+				<p className="text-lg md:text-xl">Consultá con tu médico o médica acerca de la prevención y el tratamiento del Herpes Zóster</p>
+			</div>
 		</section>
 	)
 }
--- components/home/Blog.tsx
+++ components/home/Blog.tsx
@@ -10,7 +10,7 @@ const Blog = () => {
 				<Description title={`Conocé más sobre el Herpes Zóster.`}/>
 
 				<div className="py-6">
-					<Cta title={`Leer notas`} url={`/blog`}/>
+					<Cta title={`Leer más`} url={`/blog`}/>
 				</div>
 			</div>
 		</section>
