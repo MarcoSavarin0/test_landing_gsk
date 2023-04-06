4f74f0f1444eb62b9716c9c0e2ccb28312dcc0c6
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-04-05 23:15:48 -0300

    chore: deleted extra tracking tags (keep tagmanager)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,7 @@ class MyDocument extends Document {
 				<Head>
 					<link rel="shortcut icon" href="/favicon.png" />
 
-					<Script id="google-analytics" strategy="afterInteractive">
+					<Script id="google-tagmanager" strategy="afterInteractive">
 						{`
 						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -22,41 +22,9 @@ class MyDocument extends Document {
 						})(window,document,'script','dataLayer','GTM-WL5WP7P');
 					`}
 					</Script>
-
-					<Script id="meta-pixel">
-						{
-							`
-								!function(f,b,e,v,n,t,s)
-								{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-								n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-								if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-								n.queue=[];t=b.createElement(e);t.async=!0;
-								t.src=v;s=b.getElementsByTagName(e)[0];
-								s.parentNode.insertBefore(t,s)}(window, document,'script',
-								'https://connect.facebook.net/en_US/fbevents.js');
-								fbq('init', '1929643777386048');
-								fbq('track', 'PageView');
-							`
-						}
-					</Script>
-
-					<Script id="meta-pixel-img">
-						{
-							`
-								<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1929643777386048&ev=PageView&noscript=1"/>
-							`
-						}
-					</Script>
-
-					{/* <Script id="analytics" async src="https://www.googletagmanager.com/gtag/js?id=AW-11138119089">
-						{`
-							window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}
-							gtag('js', new Date()); gtag('config', 'AW-11138119089');
-						`}
-					</Script> */}
 				</Head>
 				<body>
-					<Script id="google-analytics" strategy="afterInteractive">
+					<Script id="google-tagmanager" strategy="afterInteractive">
 						{`
 						<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WL5WP7P" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 						`}
