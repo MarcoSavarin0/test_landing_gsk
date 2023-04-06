5b2fa67aeaaf2a86a9557cdfa8a3fa5eaea5d6f3
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-04-06 01:15:31 -0300

    feat: add react-card-flip + delete react-transition-group

--- components/home/ui/CardIcon.tsx
+++ components/home/ui/CardIcon.tsx
@@ -1,5 +1,7 @@
+import {useState} from "react"
 import Image from 'next/image'
 import parse from "html-react-parser"
+import ReactCardFlip from "react-card-flip"
 
 import H3 from "@/components/ui/H3"
 
@@ -10,12 +12,23 @@ interface CardIcon {
 }
 
 const CardIcon = ({title, image, body}: CardIcon) => {
+	const [flipped, setFlipped] = useState(false)
+
+	const doFlip = (bool: boolean) => {
+		setFlipped(bool)
+	}
+
 	return (
-		<article className="flex flex-col w-full gap-y-6 items-center bg-white border border-gsk-gray rounded-xl shadow-md px-10 md:px-6 xl:px-10 py-8 text-gsk-orange">
-			<Image src={`/assets/images/${image}.svg`} alt={`Ícono Varicela`} width={80} height={80}/>
-			<H3 title={title}/>
-			<p className="text-black text-lg">{parse(body)}</p>
-		</article>
+		<ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
+			<article onClick={() => doFlip(true)} className="cursor-pointer flex flex-col justify-center mx-auto w-11/12 sm:w-9/12 md:w-9/12 lg:w-full h-full gap-y-6 items-center bg-white border border-gsk-gray rounded-xl shadow-md px-10 md:px-6 xl:px-10 py-14 text-gsk-orange">
+				<Image src={`/assets/images/${image}.svg`} alt={`Ícono Varicela`} width={80} height={80}/>
+				<H3 title={title}/>
+			</article>
+
+			<article onClick={() => doFlip(false)} className="cursor-pointer flex flex-col justify-center mx-auto w-11/12 sm:w-9/12 md:w-9/12 lg:w-full h-full gap-y-6 items-center bg-white border border-gsk-gray rounded-xl shadow-md px-10 md:px-6 xl:px-10 py-14 text-gsk-orange">
+				<p className="text-black text-lg">{parse(body)}</p>
+			</article>
+		</ReactCardFlip>
 	)
 }
 
