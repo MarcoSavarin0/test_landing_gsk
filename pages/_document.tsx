12ce5aefe46b3623e7e4e1dcef60072d393da7cf
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-04-05 22:11:00 -0300

    chore: update id to tagmanager script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,7 +19,7 @@ class MyDocument extends Document {
 						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-						})(window,document,'script','dataLayer','GTM-5YDSHRMYES');
+						})(window,document,'script','dataLayer','GTM-WL5WP7P');
 					`}
 					</Script>
 
@@ -58,7 +58,7 @@ class MyDocument extends Document {
 				<body>
 					<Script id="google-analytics" strategy="afterInteractive">
 						{`
-						<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5YDSHRMYES" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+						<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WL5WP7P" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 						`}
 					</Script>
 
