c3bc4aa4e9b4c6ab44ca89ba56d7e186686a597f
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-04-06 22:31:44 -0300

    chore: add barlow font to banner component

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image"
+import localFont from "@next/font/local"
+
+const barlowSemicondensedSemiBold = localFont({ src: '../../styles/fonts/barlowsemicondensed-semibold.woff2' })
 
 const Banner = () => {
 	return (
@@ -7,7 +10,7 @@ const Banner = () => {
 				<Image src="/shape-gsk-white.svg" width={80} height={60} alt="" />
 			</div>
 			<div className="space-y-2 px-6">
-				<h2 className="text-xl md:text-3xl leading-relaxed uppercase font-bold">Activemos antes de que el virus se reactive</h2>
+				<h2 className={`${barlowSemicondensedSemiBold.className} text-2xl md:text-4xl leading-relaxed uppercase font-bold`}>Activemos antes de que el virus se reactive</h2>
 				<p className="text-lg md:text-xl">Consultá con tu médico o médica acerca de la prevención y el tratamiento del Herpes Zóster</p>
 			</div>
 		</section>
