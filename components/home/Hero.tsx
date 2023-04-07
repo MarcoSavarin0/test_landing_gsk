0731fc8f8680c1494ba97e614c27593e74d31422
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-04-07 18:23:12 -0300

    chore: add legal testimonial + fix css and text

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -24,31 +24,31 @@ interface Dolor {
 
 const dolores: Dolor[] = [
 	{
-		title: `<span className="md:block ${barlowSemicondensedMedium.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO UN <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">ARDOR DESESPERANTE</span>"`,
+		title: `<span className="md:block ${barlowSemicondensedMedium.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO UN <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">ARDOR <span className="md:block">DESESPERANTE*</span></span>`,
 		type: "ardor",
 	},
 	{
-		title: `<span className="md:block ${barlowSemicondensedMedium.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">CLAVOS ATRAVESANDO</span> <span className="md:block">LA <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">PIEL</span></span>`,
+		title: `<span className="md:block ${barlowSemicondensedMedium.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">CLAVOS ATRAVESANDO</span> <span className="md:block">LA <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">PIEL*</span></span>`,
 		type: "clavos",
 	},
 	{
-		title: `<span className="md:block ${barlowSemicondensedMedium.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">QUEMARSE</span> CON AGUA <span className="md:block"><span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">HIRVIENDO</span></span>`,
+		title: `<span className="md:block ${barlowSemicondensedMedium.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">QUEMARSE</span> CON AGUA <span className="md:block"><span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">HIRVIENDO*</span></span>`,
 		type: "quemadura",
 	},
 	{
-		title: `<span className="md:block ${barlowSemicondensedMedium.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">FUEGO RECORRIENDO</span> <span className="md:block">LOS <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">NERVIOS</span></span>`,
+		title: `<span className="md:block ${barlowSemicondensedMedium.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">FUEGO RECORRIENDO</span> <span className="md:block">LOS <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">NERVIOS*</span></span>`,
 		type: "llamas",
 	},
 	{
-		title: `<span className="md:block ${barlowSemicondensedMedium.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">DESCARGAS ELÉCTRICAS</span> <span className="md:block">EN EL <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">CUERPO</span></span>`,
+		title: `<span className="md:block ${barlowSemicondensedMedium.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">DESCARGAS ELÉCTRICAS</span> <span className="md:block">EN EL <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">CUERPO*</span></span>`,
 		type: "electricidad",
 	},
 	{
-		title: `<span className="md:block ${barlowSemicondensedMedium.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO UNA <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">PUNTADA <span className="md:block">PERFORANTE</span></span>`,
+		title: `<span className="md:block ${barlowSemicondensedMedium.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO UNA <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">PUNTADA <span className="md:block">PERFORANTE*</span></span>`,
 		type: "puntada",
 	},
 	{
-		title: `<span className="md:block ${barlowSemicondensedMedium.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO TENER LA <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">PIEL</span> <span className="md:block">EN <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">LLAMAS</span></span>`,
+		title: `<span className="md:block ${barlowSemicondensedMedium.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO TENER LA <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">PIEL</span> <span className="md:block">EN <span className="font-bold ${barlowSemicondensedExtraBoldItalic.className}">LLAMAS*</span></span>`,
 		type: "fuego",
 	}
 ]
@@ -78,10 +78,10 @@ const Hero = () => {
 
 							<VideoHero type={type}/>
 
-							<article className="order-first absolute top-4 md:top-[20%] text-right right-8 md:right-10 lg:right-14 w-10/12 sm:w-fit md:w-7/12 lg:w-6/12 xl:w-7/12 pt-6 md:pt-0 space-y-4">
+							<article className="order-first absolute top-4 md:top-[20%] text-right right-8 md:right-10 lg:right-14 xl:right-18 2xl:right-24 w-10/12 md:w-7/12 lg:w-6/12 xl:w-7/12 pt-6 md:pt-0 space-y-4">
 								<h1 className={`${barlowSemicondensedBoldItalic.className} text-white leading-none uppercase text-4xl sm:text-6xl lg:text-7xl xl:text-8xl`}>Herpes Zóster</h1>
 								<div className="space-y-2 sm:space-y-4">
-									<h2 className={`${barlowSemicondensedMediumItalic.className} drop-shadow-[14px_18px_3px_rgba(255,255,255,0.45)] sm:drop-shadow-[12px_12px_3px_rgba(255,255,255,0.45)] uppercase inline text-white w-full sm:w-fit text-3xl sm:text-4xl md:text-4xl xl:text-5xl 2xl:text-6xl md:leading-[2rem]`}>
+									<h2 className={`${barlowSemicondensedMediumItalic.className} h-36 drop-shadow-[14px_18px_3px_rgba(255,255,255,0.45)] sm:drop-shadow-[12px_12px_3px_rgba(255,255,255,0.45)] uppercase inline text-white w-full sm:w-fit text-3xl sm:text-4xl md:text-4xl xl:text-5xl 2xl:text-6xl md:leading-[2rem]`}>
 										{parse(title)}
 									</h2>
 									<div className={`${gskPrecisionLight} hidden md:block text-gsk-orange text-xl 2xl:text-3xl font-light space-y-3`}>
@@ -92,6 +92,7 @@ const Hero = () => {
 										</div>
 									</div>
 								</div>
+								<span className="aboslute bottom-0 right-0 text-white z-20 text-[0.60rem]">*BASADO EN TESTIMONIOS REALES DE PACIENTES. LOS SÍNTOMAS DEL HERPES ZOSTER PUEDEN VARIAR.</span>
 							</article>
 
 							<div className="absolute bottom-0 left-0 w-full h-4 bg-gradient-to-t from-gsk-dark via-gsk-dark/60 to-gsk-dark/0 z-10"/>
