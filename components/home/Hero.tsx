45d31b585767510f297dc564e20e4d2fbffda663
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-04-08 08:04:32 -0300

    chore: update mobile css hero videos framing

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -20,36 +20,44 @@ const barlowSemicondensedRegular = localFont({ src: '../../styles/fonts/barlowse
 interface Dolor {
 	title: string
 	type: string
+	top: string
 }
 
 const dolores: Dolor[] = [
 	{
 		title: `<span className="md:block ${barlowSemicondensedRegular.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO UN <span className="font-bold ${barlowSemicondensedBoldItalic.className}">ARDOR <span className="md:block">DESESPERANTE*</span></span>`,
 		type: "ardor",
+		top: "-mt-16",
 	},
 	{
 		title: `<span className="md:block ${barlowSemicondensedRegular.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO <span className="font-bold ${barlowSemicondensedBoldItalic.className}">CLAVOS ATRAVESANDO</span> <span className="md:block">LA <span className="font-bold ${barlowSemicondensedBoldItalic.className}">PIEL*</span></span>`,
 		type: "clavos",
+		top: "-mt-28",
 	},
 	{
 		title: `<span className="md:block ${barlowSemicondensedRegular.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO <span className="font-bold ${barlowSemicondensedBoldItalic.className}">QUEMARSE</span> CON AGUA <span className="md:block"><span className="font-bold ${barlowSemicondensedBoldItalic.className}">HIRVIENDO*</span></span>`,
 		type: "quemadura",
+		top: "-mt-24",
 	},
 	{
 		title: `<span className="md:block ${barlowSemicondensedRegular.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO <span className="font-bold ${barlowSemicondensedBoldItalic.className}">FUEGO RECORRIENDO</span> <span className="md:block">LOS <span className="font-bold ${barlowSemicondensedBoldItalic.className}">NERVIOS*</span></span>`,
 		type: "llamas",
+		top: "-mt-8",
 	},
 	{
 		title: `<span className="md:block ${barlowSemicondensedRegular.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO <span className="font-bold ${barlowSemicondensedBoldItalic.className}">DESCARGAS ELÉCTRICAS</span> <span className="md:block">EN EL <span className="font-bold ${barlowSemicondensedBoldItalic.className}">CUERPO*</span></span>`,
 		type: "electricidad",
+		top: "-mt-8",
 	},
 	{
 		title: `<span className="md:block ${barlowSemicondensedRegular.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO UNA <span className="font-bold ${barlowSemicondensedBoldItalic.className}">PUNTADA <span className="md:block">PERFORANTE*</span></span>`,
 		type: "puntada",
+		top: "-mt-24",
 	},
 	{
 		title: `<span className="md:block ${barlowSemicondensedRegular.className}">UN DOLOR QUE PUEDE SENTIRSE</span> COMO TENER LA <span className="font-bold ${barlowSemicondensedBoldItalic.className}">PIEL</span> <span className="md:block">EN <span className="font-bold ${barlowSemicondensedBoldItalic.className}">LLAMAS*</span></span>`,
 		type: "fuego",
+		top: "-mt-10",
 	}
 ]
 
@@ -71,12 +79,12 @@ const Hero = () => {
 					stopOnLastSlide: false,
 				}}
 			>
-				{dolores.map(({title, type}: Dolor, index: number) => (
+				{dolores.map(({title, type, top}: Dolor, index: number) => (
 					<SwiperSlide key={index}>
 						<div className="relative w-full flex flex-col bg-gsk-dark h-[21rem] md:h-[27rem] lg:h-[34rem] xl:h-[46rem] 2xl:h-[54rem]">
 							<div className="hidden md:block static md:absolute top-0 left-0 w-full h-2 md:h-10 bg-gradient-to-b from-gsk-dark via-gsk-dark/60 to-gsk-dark/0 mt-4 md:mt-0 z-10"/>
 
-							<VideoHero type={type}/>
+							<VideoHero type={type} top={top}/>
 
 							<article className="order-first absolute top-4 md:top-[12%] lg:top-[20%] text-right right-8 md:right-10 lg:right-14 xl:right-18 2xl:right-24 w-10/12 md:w-7/12 lg:w-6/12 xl:w-7/12 pt-6 md:pt-0 space-y-4">
 								<h1 className={`${barlowSemicondensedBoldItalic.className} text-white leading-none uppercase text-4xl sm:text-6xl lg:text-7xl xl:text-8xl`}>Herpes Zóster</h1>
--- components/home/ui/VideoHero.tsx
+++ components/home/ui/VideoHero.tsx
@@ -1,17 +1,15 @@
-import Image from "next/image"
-
 interface VideoHero {
 	type: string
+	top: string
 }
 
-const VideoHero = ({type}: VideoHero) => {
+const VideoHero = ({type, top}: VideoHero) => {
 	return (
-		<div className="absolute bottom-auto md:bottom-0 md:left-0 w-full h-fit -mt-2 md:-mt-0">
-			<div className="visible md:hidden absolute top-0 left-0 w-full h-2 md:h-10 bg-gradient-to-b from-gsk-dark via-gsk-dark/60 to-gsk-dark/0 mt-2 md:mt-0 z-10"/>
+		<div className="absolute bottom-auto md:bottom-0 md:left-0 w-full h-fit">
+			<div className="visible md:hidden absolute top-0 left-0 w-full h-2 md:h-10 bg-gradient-to-b from-gsk-dark via-gsk-dark/60 to-gsk-dark/0 md:mt-0 z-10"/>
 
-			<video className="w-full h-[27rem] md:h-auto object-cover md:object-contain object-top aspect-video brightness-50 md:brightness-100 -mt-20 md:mt-0 lg:-mt-10 xl:-mt-20 2xl:-mt-40 bg-gsk-dark" playsInline={true} crossOrigin="anonymous" preload="metadata" muted={true} width="1280" height="700" autoPlay={true} controls={false} loop={true}>
+			<video className={`w-full h-[27rem] md:h-auto object-cover md:object-contain object-[30%] md:object-top aspect-video brightness-75 md:brightness-100 ${top} md:mt-0 lg:-mt-10 xl:-mt-20 2xl:-mt-40 bg-gsk-dark`} poster={`/assets/images/hero/${type}.jpg`} playsInline={true} crossOrigin="anonymous" preload="metadata" muted={true} width="1280" height="700" autoPlay={true} controls={false} loop={true}>
 				<source src={`/assets/videos/${type}.mp4`} type="video/mp4"/>
-				<Image src={`/assets/images/hero/${type}.jpg`} alt={`Herpes Zóster`} width="1280" height="700" className="block w-full h-full object-cover object-center"/>
 			</video>
 
 			<div className="relative md:absolute bottom-3 sm:bottom-2 md:bottom-3 lg:bottom-3 xl:bottom-5 2xl:bottom-6 left-0 w-full h-3 2xl:h-12 bg-gradient-to-t from-gsk-dark via-gsk-dark/60 to-gsk-dark/0 z-10"/>
