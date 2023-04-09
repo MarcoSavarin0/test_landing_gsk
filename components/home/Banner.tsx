94121fbf3f2a5b1feabc1fa79adc6961b858c81b
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-04-09 15:35:36 -0300

    chore: update italic fonts to banner component

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image"
 import localFont from "@next/font/local"
 
-const barlowSemicondensedBold = localFont({ src: '../../styles/fonts/barlowsemicondensed-bold.woff2' })
-const barlowSemicondensedRegular = localFont({ src: '../../styles/fonts/barlowsemicondensed-regular.woff2' })
+const barlowSemicondensedBoldItalic = localFont({ src: '../../styles/fonts/barlowsemicondensed-bolditalic.woff2' })
+const barlowSemicondensedItalic = localFont({ src: '../../styles/fonts/barlowsemicondensed-italic.woff2' })
 
 const Banner = () => {
 	return (
@@ -11,7 +11,7 @@ const Banner = () => {
 				<Image src="/shape-gsk-white.svg" width={80} height={60} alt="" />
 			</div>
 			<div className="space-y-2 px-6">
-				<h2 className={`${barlowSemicondensedRegular.className} text-2xl md:text-4xl leading-relaxed uppercase font-bold`}><span className={`${barlowSemicondensedBold.className} text-4xl md:text-5xl`}>Activemos</span> <span className="block">antes de que el <span className={`${barlowSemicondensedBold.className}`}>virus se reactive</span></span></h2>
+				<h2 className={`${barlowSemicondensedItalic.className} text-2xl md:text-4xl leading-relaxed uppercase font-bold`}><span className={`${barlowSemicondensedBoldItalic.className} text-4xl md:text-5xl`}>Activemos</span> <span className="block">antes de que el <span className={`${barlowSemicondensedBoldItalic.className}`}>virus se reactive</span></span></h2>
 				<p className="text-lg md:text-xl">Consultá con tu médico o médica acerca de la prevención y el tratamiento del Herpes Zóster</p>
 			</div>
 		</section>
