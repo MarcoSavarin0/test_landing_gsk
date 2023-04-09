3ddf9b135ad33d9d305e3bd11be796db69585aa9
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-04-09 15:28:02 -0300

    chore: update css responsive to title hero

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -23,37 +23,37 @@ interface Dolor {
 
 const dolores: Dolor[] = [
 	{
-		title: `<span className="md:block">PUEDE SENTIRSE COMO UN</span><span className="md:block font-bold ${barlowSemicondensedBoldItalic.className}">ARDOR DESESPERANTE*</span>`,
+		title: `<span className="md:block">PUEDE SENTIRSE COMO UN</span><span className="block"><span className="font-bold ${barlowSemicondensedBoldItalic.className}">ARDOR DESESPERANTE</span>*</span>`,
 		type: "ardor",
 		top: "-mt-16",
 	},
 	{
-		title: `<span className="md:block">PUEDE SENTIRSE COMO</span> <span className="font-bold ${barlowSemicondensedBoldItalic.className}">CLAVOS ATRAVESANDO</span> <span className="md:block">LA <span className="font-bold ${barlowSemicondensedBoldItalic.className}">PIEL*</span></span>`,
+		title: `<span className="md:block">PUEDE SENTIRSE COMO</span> <span className="font-bold ${barlowSemicondensedBoldItalic.className}">CLAVOS ATRAVESANDO</span> <span className="block">LA <span className="font-bold ${barlowSemicondensedBoldItalic.className}">PIEL</span>*</span>`,
 		type: "clavos",
 		top: "-mt-24",
 	},
 	{
-		title: `<span className="md:block">PUEDE SENTIRSE COMO</span> <span className="font-bold ${barlowSemicondensedBoldItalic.className}">QUEMARSE</span> CON AGUA <span className="md:block"><span className="font-bold ${barlowSemicondensedBoldItalic.className}">HIRVIENDO*</span></span>`,
+		title: `<span className="md:block">PUEDE SENTIRSE COMO</span> <span className="font-bold ${barlowSemicondensedBoldItalic.className}">QUEMARSE</span> CON AGUA <span className="block"><span className="font-bold ${barlowSemicondensedBoldItalic.className}">HIRVIENDO</span>*</span>`,
 		type: "quemadura",
 		top: "-mt-24",
 	},
 	{
-		title: `<span className="md:block">PUEDE SENTIRSE COMO</span> <span className="font-bold ${barlowSemicondensedBoldItalic.className}">FUEGO RECORRIENDO</span> <span className="md:block">LOS <span className="font-bold ${barlowSemicondensedBoldItalic.className}">NERVIOS*</span></span>`,
+		title: `<span className="block">PUEDE SENTIRSE COMO</span> <span className="font-bold ${barlowSemicondensedBoldItalic.className}">FUEGO RECORRIENDO</span> <span className="block">LOS <span className="font-bold ${barlowSemicondensedBoldItalic.className}">NERVIOS</span>*</span>`,
 		type: "llamas",
 		top: "-mt-8",
 	},
 	{
-		title: `<span className="md:block">PUEDE SENTIRSE COMO</span> <span className="font-bold ${barlowSemicondensedBoldItalic.className}">DESCARGAS ELÉCTRICAS</span> <span className="md:block">EN EL <span className="font-bold ${barlowSemicondensedBoldItalic.className}">CUERPO*</span></span>`,
+		title: `<span className="block">PUEDE SENTIRSE COMO</span> <span className="font-bold ${barlowSemicondensedBoldItalic.className}">DESCARGAS ELÉCTRICAS</span> <span className="block">EN EL <span className="font-bold ${barlowSemicondensedBoldItalic.className}">CUERPO</span>*</span>`,
 		type: "electricidad",
 		top: "-mt-8",
 	},
 	{
-		title: `<span className="md:block">PUEDE SENTIRSE COMO UNA</span> <span className="font-bold ${barlowSemicondensedBoldItalic.className}">PUNTADA PERFORANTE*</span>`,
+		title: `<span className="block">PUEDE SENTIRSE COMO UNA</span> <span className="font-bold ${barlowSemicondensedBoldItalic.className}">PUNTADA PERFORANTE</span>*`,
 		type: "puntada",
 		top: "-mt-24",
 	},
 	{
-		title: `<span className="md:block">PUEDE SENTIRSE COMO</span> TENER LA <span className="font-bold ${barlowSemicondensedBoldItalic.className}">PIEL</span> <span className="md:block">EN <span className="font-bold ${barlowSemicondensedBoldItalic.className}">LLAMAS*</span></span>`,
+		title: `<span className="block">PUEDE SENTIRSE COMO</span> TENER LA <span className="font-bold ${barlowSemicondensedBoldItalic.className}">PIEL</span> <span className="block">EN <span className="font-bold ${barlowSemicondensedBoldItalic.className}">LLAMAS</span>*</span>`,
 		type: "fuego",
 		top: "-mt-10",
 	}
@@ -79,7 +79,7 @@ const Hero = () => {
 			>
 				{dolores.map(({title, type, top}: Dolor, index: number) => (
 					<SwiperSlide key={index}>
-						<div className="relative w-full flex flex-col bg-gsk-dark h-[21rem] md:h-[27rem] lg:h-[34rem] xl:h-[46rem] 2xl:h-[54rem]">
+						<div className="relative w-full flex flex-col bg-gsk-dark h-[21rem] md:h-[27rem] lg:h-[34rem] xl:h-[46rem] 2xl:h-[54rem] overflow-hidden">
 							<div className="hidden md:block static md:absolute top-0 left-0 w-full h-2 md:h-10 bg-gradient-to-b from-gsk-dark via-gsk-dark/60 to-gsk-dark/0 mt-4 md:mt-0 z-10"/>
 
 							<VideoHero type={type} top={top}/>
@@ -100,7 +100,6 @@ const Hero = () => {
 										</div>
 									</div>
 								</div>
-
 							</article>
 
 							<span className="absolute bottom-6 md:bottom-10 xl:bottom-14 right-8 md:right-10 lg:right-14 2xl:right-24 text-white z-20 text-[0.50rem] text-right w-fit leading-tight">*BASADO EN TESTIMONIOS REALES DE PACIENTES. <span className="block lg:inline">LOS SÍNTOMAS DEL HERPES ZOSTER PUEDEN VARIAR.</span></span>
@@ -111,7 +110,7 @@ const Hero = () => {
 				))}
 			</Swiper>
 
-			<div className={`${gskPrecisionLight} bg-gsk-dark block md:hidden text-gsk-orange text-xl 2xl:text-3xl font-light px-8 md:px-0 my-4`}>
+			<div className={`${gskPrecisionLight.className} bg-gsk-dark block md:hidden text-gsk-orange text-xl 2xl:text-3xl font-light px-8 md:px-0 my-4`}>
 				<p>Conocé más sobre la enfermedad, sus síntomas <span className="lg:block">y las opciones de prevención y tratamiento.</span></p>
 				<p><Link href={`/#que-es-herpes-zoster`} scroll={false} className="hover:text-white transition ease-out duration-300">Hablemos de Zóster</Link>.</p>
 			</div>
--- components/home/ui/VideoHero.tsx
+++ components/home/ui/VideoHero.tsx
@@ -8,7 +8,7 @@ const VideoHero = ({type, top}: VideoHero) => {
 		<div className="absolute bottom-auto md:bottom-0 md:left-0 w-full h-fit">
 			<div className="visible md:hidden absolute top-0 left-0 w-full h-2 md:h-10 bg-gradient-to-b from-gsk-dark via-gsk-dark/60 to-gsk-dark/0 md:mt-0 z-10"/>
 
-			<video className={`w-full h-[27rem] md:h-auto object-cover md:object-contain object-[30%] md:object-top aspect-video brightness-75 md:brightness-100 ${top} md:mt-0 lg:-mt-10 xl:-mt-20 2xl:-mt-40 bg-gsk-dark`} poster={`/assets/images/hero/${type}.jpg`} playsInline={true} crossOrigin="anonymous" preload="metadata" muted={true} width="1280" height="700" autoPlay={true} controls={false} loop={true}>
+			<video className={`w-full h-[27rem] md:h-auto object-cover md:object-contain object-[30%] md:object-top aspect-video brightness-75 md:brightness-100 ${top} md:mt-0 bg-gsk-dark`} poster={`/assets/images/hero/${type}.jpg`} playsInline={true} crossOrigin="anonymous" preload="metadata" muted={true} width="1280" height="700" autoPlay={true} controls={false} loop={true}>
 				<source src={`/assets/videos/${type}.mp4`} type="video/mp4"/>
 			</video>
 
