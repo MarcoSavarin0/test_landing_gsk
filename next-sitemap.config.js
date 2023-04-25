f733a86e1daa6101cb2d876a6cba6430a1d29ea3
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-04-25 11:50:02 -0300

    chore: update sitemap url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-	siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://hablemosdezoster.com',
+	siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://www.hablemosdezoster.com',
 	generateRobotsTxt: true,
 }
