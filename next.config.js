537178ec2745711bad6cf80c57d67a4b8b0ad5ab
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-05-08 13:51:38 -0300

    chore: edit blog images cdn to bunny-cdn config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: [`${process.env.NEXT_PUBLIC_AWS_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_AWS_PUBLIC_REGION}.amazonaws.com`, `gsk-hdz.b-cdn.net`, `b-cdn.net`, `br.storage.bunnycdn.com`],
+		domains: [
+			`${process.env.NEXT_PUBLIC_AWS_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_AWS_PUBLIC_REGION}.amazonaws.com`, `gsk-hdz.b-cdn.net`
+		],
 	},
 }
 
