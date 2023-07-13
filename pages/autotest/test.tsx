35ad3ee9ace3b4e39299d30251f73a8e3858c3fe
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-07-13 04:49:48 -0300

    chore: add css to test page

--- pages/autotest/test.tsx
+++ pages/autotest/test.tsx
@@ -1,29 +1,43 @@
+import H2 from "@/components/ui/H2"
+import Banner from "@/components/home/Banner"
+
 const Autotest = () => {
 	return (
-		<>
-			<h1>HERPES ZÓSTER</h1>
-			<h2>TEST: ¿ESTOY EN RIESGO DE TENER HERPES ZÓSTER?</h2>
-
+		<section className="space-y-6 md:space-y-32 bg-gsk-dark">
 			<div>
-				<span>1/4</span>
-
-				<div>
-					<span/>
-					<span/>
-					<span/>
-					<span/>
+				<div className="space-y-3 px-4 text-center  pt-10 pb-36 shadow-xl">
+					<h1 className={`text-gsk-orange font-bold leading-none uppercase text-4xl sm:text-5xl lg:text-6xl xl:text-7xl`}>Herpes Zóster</h1>
+					<H2 title={`TEST: ¿ESTOY EN RIESGO DE TENER HERPES ZÓSTER?`}/>
 				</div>
 
-				<p>PREGUNTA 1</p>
+				<article className="w-11/12 md:w-10/12 mx-auto about px-10 space-y-14 text-white rounded-xl">
+					<div className="-mt-24 pt-8 space-y-5">
+						<div>
+							<span className="text-xs">1 / 4</span>
 
-				<div>
-					<button>SÍ</button>
-					<button>NO</button>
-				</div>
+							<div className="flex flex-wrap gap-x-1 justify-start items-center">
+								<span className="bg-gsk-orange w-8 sm:w-12 md:w-16 lg:w-20 h-1"/>
+								<span className="bg-gsk-gray w-8 sm:w-12 md:w-16 lg:w-20 h-1"/>
+								<span className="bg-gsk-gray w-8 sm:w-12 md:w-16 lg:w-20 h-1"/>
+								<span className="bg-gsk-gray w-8 sm:w-12 md:w-16 lg:w-20 h-1"/>
+							</div>
+						</div>
 
-				<button>SIGUIENTE</button>
+						<p className="uppercase text-xl w-full md:w-10/12 lg:w-1/2 font-bold">¿SABÍAS que el <span className="text-gsk-orange">Herpes Zóster</span> es causado por el virus <span className="text-gsk-orange">Varicela-Zóster</span>, el mismo que ocasiona la varicela?</p>
+
+						<div className="flex flex-wrap gap-x-2 gap-y-2">
+							<button className="px-4 py-1 rounded-lg  uppercase border border-white hover:border-gsk-orange bg-gsk-dark hover:bg-gsk-orange transition-colors duration-200">SÍ</button>
+							<button className="px-4 py-1 rounded-lg  uppercase border border-white hover:border-gsk-orange bg-gsk-dark hover:bg-gsk-orange transition-colors duration-200">NO</button>
+							<button className="px-4 py-1 rounded-lg  uppercase border border-white hover:border-gsk-orange bg-gsk-dark hover:bg-gsk-orange transition-colors duration-200">NO LO RECUERDO</button>
+						</div>
+
+						<button className="px-4 py-1 rounded-lg uppercase border border-gsk-orange hover:border-white bg-gsk-orange hover:bg-white text-white hover:text-gsk-orange transition-colors duration-200">SIGUIENTE</button>
+					</div>
+				</article>
 			</div>
-		</>
+
+			<Banner/>
+		</section>
 	)
 }
 
