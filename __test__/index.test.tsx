88b027084dadfa48e3f91ca6cab6dc9383bbb03c
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-08-09 23:55:39 -0300

    chore: upadte index test

--- __test__/index.test.tsx
+++ __test__/index.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react'
 import Intro from "@/components/home/Intro"
 import '@testing-library/jest-dom'
 
-describe('Home', () => {
+describe('Hero', () => {
 	it('renders a title', () => {
 		render(<Intro />)
 	})
