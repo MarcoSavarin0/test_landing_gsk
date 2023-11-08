48966601d2375862cdfe7f2c5b7f4e85b7f9fb94
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-11-08 09:00:23 -0300

    chore: add usestate video selector for testimonios comp

--- components/home/Testimonial.tsx
+++ components/home/Testimonial.tsx
@@ -1,3 +1,4 @@
+import {useState, useEffect} from "react"
 import dynamic from 'next/dynamic'
 import H2 from "@/components/ui/H2"
 import ButtonThumbnail from "@/components/home/ui/ButtonThumbnail"
@@ -8,7 +9,7 @@ interface VideoTestimonio {
 	name: string
 	data: string
 	active: boolean
-	video: string
+	id: string
 }
 
 const medicos: VideoTestimonio[] = [
@@ -16,30 +17,41 @@ const medicos: VideoTestimonio[] = [
 		name: "Dra. Elena Obieta",
 		data: "Médica Infectóloga",
 		active: true,
-		video: "dIWt8S179ng",
+		id: "dIWt8S179ng",
 	},
 	{
 		name: "Dra. Florencia Cahn",
 		data: "Médica Infectóloga",
 		active: false,
-		video: "vBruLfIo-FE",
+		id: "vBruLfIo-FE",
 	},
 	{
 		name: "Dr. Lucio Criado",
 		data: "Médico Clínico",
 		active: false,
-		video: "vgK0G2UWif4",
+		id: "vgK0G2UWif4",
 	},
 	{
 		name: "Dr. Roberto Rey",
 		data: "Neurólogo",
 		active: false,
-		video: "0rBIlJ7FP04",
+		id: "0rBIlJ7FP04",
 	},
 
 ]
 
 const Testimonial = () => {
+	const [playerId, setPlayerId] = useState("dIWt8S179ng")
+
+	const selectVideo = (playerId: string, indexVideo: number) => {
+		medicos[indexVideo].active = true
+		setPlayerId(playerId)
+	}
+
+	useEffect(() => {
+		setPlayerId(playerId)
+	}, [playerId])
+
 	return (
 		<section className="space-y-10 bg-gsk-gray pt-6 pb-10 drop-shadow-[0_0_15px_rgba(0,0,0,0.10)] mb-10">
 			<div className="text-center space-y-3 px-4">
@@ -48,11 +60,13 @@ const Testimonial = () => {
 
 			<div className="flex flex-col px-12 space-y-6 justify-center items-center">
 				{/* <button>|</button> */}
-				<VideoplayerSSR id={`dIWt8S179ng`}/>
+				<VideoplayerSSR id={playerId}/>
 
 				<div className="grid grid-cols-2 md:grid-cols-4 w-full gap-6 justify-between text-center px-2">
 					{medicos.map((video, index) => (
-						<ButtonThumbnail key={index} name={video.name} data={video.data} active={video.active}/>
+						<button key={index} onClick={() => selectVideo(video.id, index)}>
+							<ButtonThumbnail name={video.name} data={video.data} active={video.active}/>
+						</button>
 					))}
 				</div>
 			</div>
--- components/home/ui/ButtonThumbnail.tsx
+++ components/home/ui/ButtonThumbnail.tsx
@@ -8,14 +8,14 @@ interface VideoTestimonio {
 
 const ButtonThumbnail = ({name, data, active}: VideoTestimonio) => {
 	return (
-		<button className="group relative hover:shadow-lg transition ease-out duration-200 overflow-hidden w-full h-44 flex flex-col justify-center items-center rounded-xl px-4 md:px-6 py-8">
+		<article className="group relative hover:shadow-lg transition ease-out duration-200 overflow-hidden w-full h-44 flex flex-col justify-center items-center rounded-xl px-4 md:px-6 py-8">
 			<div className={`${active ? 'bg-gsk-orange/60' : ''} absolute top-0 left-0 w-full h-full group-hover:bg-gsk-orange/60 z-10 mix-blend-multiply transition ease-out duration-200`}/>
 			<Image className="absolute object-cover w-full h-44 top-0 left-0" alt={`Alt`} src={`/assets/images/notas/varicela.webp`} width={100} height={80}/>
 			<div className="z-10 text-white drop-shadow-[0_1px_2px_rgba(0,0,0,0.80)] z-20">
 				<h3 className="font-bold text-xl">{name}</h3>
 				<p>{data}</p>
 			</div>
-		</button>
+		</article>
 	)
 }
 
