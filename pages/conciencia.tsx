a0da49d8207ec27580c878f6b94b968f9b2c01e4
Milton Kienigiel <miltonkienigiel@gmail.com>
2024-02-25 23:55:37 -0600

    fix: saw ui differences

--- pages/conciencia.tsx
+++ pages/conciencia.tsx
@@ -32,7 +32,7 @@ const Conciencia = () => {
 			/>
 
 			<>
-				<section className="bg-[##FFFFFF] pb-20">
+				<section className="bg-[##FFFFFF] pb-10 md:pb-20">
 					<div className="w-full h-auto">
 						<Image src={"https://gsk-hdz-2.b-cdn.net/conciencia/bannerGSK.webp"} alt={"Semana de Concientización"} width={720} height={1000} className={`w-full h-auto object-cover bg-gsk-dark`}/>
 
@@ -42,13 +42,13 @@ const Conciencia = () => {
 					</div>
 
 					<article className="w-11/12 md:w-10/12 mx-auto about px-8 space-y-14 rounded-xl ">
-						<div className="pt-20 space-y-12">
+						<div className="pt-10 md:pt-20 space-y-12">
 
 							<div className="flex flex-col lg:flex-row gap-y-8 lg:gap-y-0 gap-x-8">
 								<div className="space-y-8 w-full lg:w-7/12 xl:w-7/12 2xl:w-9/12">
 									<h1 className={`text-gsk-blue text-2xl md:text-4xl leading-relaxed  font-bold`}>Semana de Concientización sobre el Herpes Zóster</h1>
 									<div className="space-y-6 text-justify">
-										<p>En el marco de la Semana de Concientización del Herpes Zóster, que se celebra entre el 26 de febrero y el 3 de marzo, buscamos generar conciencia sobre los riesgos y el impacto de esta patología. Se calcula que más del 90% de los adultos mayores de 50 años, han sido infectados por el virus que causa la varicela y, por lo tanto, están en riesgo de que se reactive, desarrollando Herpes Zóster.</p>
+										<p>En el marco de la Semana de Concientización del Herpes Zóster (Shingles Awareness Week o SAW, por sus siglas en inglés), que se celebra entre el 26 de febrero y el 3 de marzo, buscamos generar conciencia sobre los riesgos y el impacto de esta patología. Se calcula que más del 90% de los adultos mayores de 50 años, han sido infectados por el virus que causa la varicela y, por lo tanto, están en riesgo de que se reactive, desarrollando Herpes Zóster.</p>
 										<p>Cualquier persona que haya tenido varicela puede desarrollar Herpes Zóster, incluso aquellas que llevan un estilo de vida saludable.  </p>
 										{/* <p>Horas después de la aparición de hZ_90 en las redes de ambas figuras, las pantallas que rodean al Obelisco, en la concurrida Avenida 9 de Julio, comenzaron a mostrar un mensaje en simultáneo que se repetía una y otra vez: hz_90 aseguraba que todas esas pantallas ahora eran suyas. Lo mismo pasaba en otras zonas de la Capital Federal, en las líneas de subte de la Ciudad de Buenos Aires y en las pantallas de los principales shoppings del país.</p>
 										<p>Mientras esto sucedía, el tema despertó el interés en redes sociales y en los principales programas de TV y radio, donde crecía la incógnita acerca de qué se trataba hZ_90.</p>
@@ -110,21 +110,21 @@ const Conciencia = () => {
 					</article>
 
 				</section>
-				<section className="w-full h-auto bg-[#00B8AC] pb-20">
+				<section className="w-full h-auto bg-[#00B8AC] pb-10 md:pb-20">
 					<div className="w-11/12 md:w-10/12 mx-auto about px-8 space-y-14 rounded-xl ">
-						<h2 className="text-white font-bold text-2xl md:text-3xl pt-20">Queremos concientizar sobre qué es el Herper Zóster</h2>
+						<h2 className="text-white font-bold text-2xl md:text-3xl pt-10 md:pt-20">Queremos concientizar sobre qué es el Herper Zóster</h2>
 						<p>El Herpes Zóster, también conocido como culebrilla, es una enfermedad causada por la reactivación del virus varicela-zóster (VVZ), el mismo virus que causa la varicela. Luego de que una persona contrae Varicela, el virus permanece latente en su sistema nervioso. A mayor edad se produce un debilitamiento del sistema inmune que puede aumentar el riesgo de que el virus se reactive. </p>
 					</div>
 				</section>
 				<section className="w-full h-auto bg-[#F8F8F8] pb-20">
 					<div className="w-11/12 md:w-10/12 mx-auto about px-8 space-y-14 rounded-xl">
-						<h2 className="text-gsk-blue font-bold text-2xl md:text-3xl pt-20">Conocé sus principales síntomas:</h2>
+						<h2 className="text-gsk-blue font-bold text-2xl md:text-3xl pt-10 md:pt-20">Conocé sus principales síntomas:</h2>
 						<p>Generalmente los primeros síntomas del Herpes Zóster son picazón, dolor, hormigueo, sensación molesta o sensibilidad al tacto en el tórax, el abdomen o el rostro. Tras los primeros días, comienza la fase aguda de la enfermedad, en la que aparece la erupción cutánea, con ampollas en las zonas afectadas, acompañas de un dolor agudo. Al cabo de unos 10 días, las ampollas se convierten en costras que, normalmente, duran entre 2 a 4 semanas. </p>
 					</div>
 				</section>
-				<section className="w-full h-auto  pb-20">
+				<section className="w-full h-auto  pb-10 md:pb-20">
 
-					<h2 className="text-gsk-black text-center font-bold text-2xl md:text-3xl pt-20">¿Estás en riesgo de desarrollar Herpes Zóster?</h2>
+					<h2 className="text-gsk-black text-center font-bold text-2xl md:text-3xl pt-10 md:pt-20">¿Estás en riesgo de desarrollar Herpes Zóster?</h2>
 					<p className="pt-10 text-gsk-blue underline text-center text-xl">Estás en riesgo si: </p>
 
 					<article className="w-11/12 md:w-10/12 mx-auto about px-8 space-y-8 rounded-xl risks_container pt-10">
@@ -147,18 +147,18 @@ const Conciencia = () => {
 					
 					</article>
 
-					<div className="flex flex-col justify-center gap-y-5 items-center text-center pt-20">
+					<div className="flex flex-col justify-center gap-y-5 items-center text-center pt-10 md:pt-20">
 						<h2 className="text-lg">Para conocer más sobre Herpes Zóster, hacé click acá: </h2>
 						<div className="flex flex-col md:flex-row justify-center items-center gap-x-0 sm:gap-x-2 gap-y-2">
-							<Link href="https://www.hablemosdezoster.com/#que-es-herpes-zoster" onClick={() => va.track('Hz90-Cta-queesherpeszoster')} className="text-base text-center text-white hover:text-gsk-blue bg-gsk-blue hover:bg-white border-2 border-transparent hover:border-gsk-blue transition duration-300 ease-out rounded px-8 py-3 uppercase font-bold">
+							<Link href="https://www.hablemosdezoster.com/#que-es-herpes-zoster" onClick={() => va.track('Hz90-Cta-queesherpeszoster')} className="w-full md:w-auto text-base text-center text-white hover:text-gsk-blue bg-gsk-blue hover:bg-white border-2 border-transparent hover:border-gsk-blue transition duration-300 ease-out rounded px-8 py-3 uppercase font-bold">
 								<h3>¿Qué es el Herpes Zoster?</h3>
 							</Link>
 
-							<Link href="https://www.hablemosdezoster.com/autotest" onClick={() => va.track('Hz90-Cta-autotest')} className="text-base text-center text-white hover:text-gsk-blue bg-gsk-blue hover:bg-white border-2 border-transparent hover:border-gsk-blue transition duration-300 ease-out rounded px-8 py-3 uppercase font-bold">
+							<Link href="https://www.hablemosdezoster.com/autotest" onClick={() => va.track('Hz90-Cta-autotest')} className="w-full md:w-auto text-base text-center text-white hover:text-gsk-blue bg-gsk-blue hover:bg-white border-2 border-transparent hover:border-gsk-blue transition duration-300 ease-out rounded px-8 py-3 uppercase font-bold">
 								<h3>Conocé tu nivel de riesgo</h3>
 							</Link>
 
-							<Link href="https://gsk-hdz.b-cdn.net/hz90/hablemos-de-zoster.pdf" onClick={() => va.track('Hz90-Cta-pdf')} target="_blank" className="text-base text-center text-white hover:text-gsk-blue bg-gsk-blue hover:bg-white border-2 border-transparent hover:border-gsk-blue transition duration-300 ease-out rounded px-8 py-3 uppercase font-bold">
+							<Link href="https://gsk-hdz.b-cdn.net/hz90/hablemos-de-zoster.pdf" onClick={() => va.track('Hz90-Cta-pdf')} target="_blank" className="w-full md:w-auto text-base text-center text-white hover:text-gsk-blue bg-gsk-blue hover:bg-white border-2 border-transparent hover:border-gsk-blue transition duration-300 ease-out rounded px-8 py-3 uppercase font-bold">
 								<h3>Descargá el folleto</h3>
 							</Link>
 						</div>
