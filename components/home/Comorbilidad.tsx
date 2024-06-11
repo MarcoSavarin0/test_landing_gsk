7da4c1f7e1210ce43fec2f1fd64fdd59d166569e
devgsk2023 <154910902+devgsk2023@users.noreply.github.com>
2024-06-11 14:33:41 -0300

    Update Comorbilidad.tsx

--- components/home/Comorbilidad.tsx
+++ components/home/Comorbilidad.tsx
@@ -15,17 +15,17 @@ interface Card {
 const cards: Card[] = [
 	{
 		title: "ENFERMEDADES RESPIRATORIAS",
-		body: "Los pacientes con enfermedades respiratorias tienen un 30% más de riesgo de desarrollar Herpes Zóster. Explorá como esta enfermedad crónica puede afectar el sistema inmunológico, dando lugar a infecciones virales.",
+		body: "Los pacientes con enfermedades respiratorias tienen un 30% más de riesgo de desarrollar Herpes Zóster. Explorá como esta enfermedad crónica puede afectar el sistema inmunológico, dando lugar a infecciones virales<sup>9<sup/>.",
 		image: "herpes-zoster-epoc",
 	},
 	{
 		title: "DIABETES",
-		body: "Los pacientes con diabetes tienen hasta un 40% más de riesgo de desarrollar Herpes Zóster. Explorá como esta enfermedad crónica puede afectar el sistema inmunológico, dando lugar a infecciones virales.",
+		body: "Los pacientes con diabetes tienen hasta un 40% más de riesgo de desarrollar Herpes Zóster. Explorá como esta enfermedad crónica puede afectar el sistema inmunológico, dando lugar a infecciones virales<sup>10<sup/>.",
 		image: "relacion-entre-diabetes-herpes-zoster",
 	},
 	{
 		title: "ENFERMEDADES CARDIOVASCULARES",
-		body: "Los pacientes con enfermedades cardiovasculares tienen hasta un 30% más de riesgo de desarrollar Herpes Zóster. Explorá como esta enfermedad crónica puede afectar el sistema inmunológico, dando lugar a infecciones virales.",
+		body: "Los pacientes con enfermedades cardiovasculares tienen hasta un 30% más de riesgo de desarrollar Herpes Zóster. Explorá como esta enfermedad crónica puede afectar el sistema inmunológico, dando lugar a infecciones virales<sup>11<sup/>.",
 		image: "corazon",
 	},
 	
