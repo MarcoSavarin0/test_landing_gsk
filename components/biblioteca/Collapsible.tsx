19b3590764a230837b7764fe8af9a2b74248a3f5
devgsk2023 <154910902+devgsk2023@users.noreply.github.com>
2024-07-01 15:39:37 -0300

    Update Collapsible.tsx

--- components/biblioteca/Collapsible.tsx
+++ components/biblioteca/Collapsible.tsx
@@ -11,9 +11,9 @@ const CollapsibleList = ({ list }: { list: string[] }) => {
 		<div>
 			<button className="text-gsk-orange text-lg" onClick={toggleList}>{isActive ? 'Ocultar Referencias' : 'Referencias'}</button>
 			{isActive && (
-				<ul>
+				<ul style={{ padding: 0 }}>
 					{list.map((item, index) => (
-						<li key={index}>{item}</li>
+						<li key={index} style={{ marginBottom: '1rem' }} dangerouslySetInnerHTML={{ __html: item }}></li>
 					))}
 				</ul>
 			)}
