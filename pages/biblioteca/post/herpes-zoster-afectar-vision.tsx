9210fb0119b6cb324d57a07555f9b49440692434
devgsk2023 <154910902+devgsk2023@users.noreply.github.com>
2024-07-19 11:41:46 -0300

    Update herpes-zoster-afectar-vision.tsx

--- pages/biblioteca/post/herpes-zoster-afectar-vision.tsx
+++ pages/biblioteca/post/herpes-zoster-afectar-vision.tsx
@@ -1,21 +1,18 @@
-import Image from "next/image";
-import { NextSeo } from "next-seo";
-import Markdown from "markdown-to-jsx";
+import Image from 'next/image'
+import { NextSeo } from "next-seo"
+import Markdown from 'markdown-to-jsx'
 
-import Banner from "@/components/home/Banner";
+import Banner from "@/components/home/Banner"
 
-import dynamic from "next/dynamic";
-import { GetStaticProps } from "next";
-import parse from "html-react-parser";
-import Link from "next/link";
-import CollapsibleList from "@/components/biblioteca/Collapsible";
+import dynamic from "next/dynamic"
+import { GetStaticProps } from "next"
+import parse from "html-react-parser"
+import Link from "next/link"
+import CollapsibleList from '@/components/biblioteca/Collapsible'
 
-const DynamicSpeechBlog = dynamic(
-  () => import("@/components/blog/SpeechBlog"),
-  {
-    ssr: false,
-  }
-);
+const DynamicSpeechBlog = dynamic(() => import('@/components/blog/SpeechBlog'), {
+	ssr: false,
+})
 
 const info = `
 **El Herpes Zóster es una afección que puede manifestarse en diferentes formas y partes del cuerpo, incluidos los ojos**. Aunque muchos están familiarizados con la clásica erupción cutánea que a menudo se asocia con esta afección, pocos son conscientes de las serias implicancias que puede tener para la vista.<sup>[1]</sup>
@@ -68,43 +65,41 @@ La aparición del Herpes Zóster en la zona ocular es una preocupación seria qu
 Si tenés síntomas de Herpes Zóster en la zona ocular, o conocés a alguien que los tenga, **es crucial que consultes con tu médico o médica lo antes posible para un diagnóstico y plan de seguimiento adecuado**.
 
 NP-AR-HZU-WCNT-230019
-`;
+`
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  // const res = await fetch(`${process.env.STRAPI_API_URL}/notas?populate=*&filters[slug][$in][0]=${context.params?.slug}`, {
-  // 	headers: {
-  // 		'Accept': 'application/json',
-  // 		'Content-Type': 'application/json',
-  // 		'Authorization': `Bearer ${process.env.STRAPI_API_TOKEN}`
-  // 	}
-  // })
-  // const nota = await res.json()
-  console.log(context);
-  const nota = {
-    data: [
-      {
-        id: 1,
-        attributes: {
-          title: "Herpes Zóster: ¿Cómo puede afectar la visión?",
-          image: "N41-_Herpes-Zóster-Cómo-puede-afectar-la-visión_-_1_",
-          metatitle:
-            "¿Cómo el Herpes Zóster puede afectar la visión? | Blog | Hablemos de Zóster",
-          metadescription:
-            " Conocé el vínculo entre Herpes Zóster y la salud ocular. Aprendé sobre los riesgos, sus síntomas y cómo tratarlos. Cuidá tu salud.",
-          slug: "note-slug",
-          categoria: "Note Category",
-          tags: ["tag1", "tag2", "tag3"],
-        },
-      },
-    ],
-  };
-  return {
-    props: {
-      nota,
-    },
-    revalidate: 10,
-  };
-};
+	// const res = await fetch(`${process.env.STRAPI_API_URL}/notas?populate=*&filters[slug][$in][0]=${context.params?.slug}`, {
+	// 	headers: {
+	// 		'Accept': 'application/json',
+	// 		'Content-Type': 'application/json',
+	// 		'Authorization': `Bearer ${process.env.STRAPI_API_TOKEN}`
+	// 	}
+	// })
+	// const nota = await res.json()
+	console.log(context)
+	const nota = {
+		"data": [
+			{
+				"id": 1,
+				"attributes": {
+					"title": "Herpes Zóster: ¿Cómo puede afectar la visión?",
+					"image": "N41-_Herpes-Zóster-Cómo-puede-afectar-la-visión_-_1_",
+					"metatitle": "¿Cómo el Herpes Zóster puede afectar la visión? | Blog | Hablemos de Zóster",
+					"metadescription": " Conocé el vínculo entre Herpes Zóster y la salud ocular. Aprendé sobre los riesgos, sus síntomas y cómo tratarlos. Cuidá tu salud.",
+					"slug": "note-slug",
+					"categoria": "Note Category",
+					"tags": ["tag1", "tag2", "tag3"]
+				}
+			}
+		]
+	}
+	return {
+		props: {
+			nota
+		},
+		revalidate: 10,
+	}
+}
 
 // export const getStaticPaths: GetStaticPaths = async () => {
 // 	// const res = await fetch(`${process.env.STRAPI_API_URL}/notas?fields[0]=slug&sort[0]=publishedAt%3Adesc&pagination[pageSize]=26`, {
@@ -159,122 +154,115 @@ const shimmer = (w: number, h: number) => `
   <rect width="${w}" height="${h}" fill="#333" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`;
+</svg>`
 
 const toBase64 = (str: string) =>
-  typeof window === "undefined"
-    ? Buffer.from(str).toString("base64")
-    : window.btoa(str);
+	typeof window === 'undefined'
+		? Buffer.from(str).toString('base64')
+		: window.btoa(str)
 
 const Blog = ({ nota }: any) => {
-  const { title, image, metatitle, metadescription, slug } =
-    nota.data[0].attributes;
+	const { title, image, metatitle, metadescription, slug } = nota.data[0].attributes
 
-  const list = [
-    `<sup>1</sup> Davis AR, Sheppard J. Herpes Zoster Ophthalmicus Review and Prevention. Eye Contact Lens. 2019 Sep;45(5):286-291. doi: 10.1097/ICL.0000000000000591. PMID: 30844951.`,
+	const list = [
+	`<sup>1</sup> Davis AR, Sheppard J. Herpes Zoster Ophthalmicus Review and Prevention. Eye Contact Lens. 2019 Sep;45(5):286-291. doi: 10.1097/ICL.0000000000000591. PMID: 30844951.`,
+	
+	`<sup>2</sup> Davis AR, Sheppard J. Herpes Zoster Ophthalmicus Review and Prevention. Eye Contact Lens. 2019 Sep;45(5):286-291. doi: 10.1097/ICL.0000000000000591. PMID: 30844951.`,
 
-    `<sup>2</sup> Davis AR, Sheppard J. Herpes Zoster Ophthalmicus Review and Prevention. Eye Contact Lens. 2019 Sep;45(5):286-291. doi: 10.1097/ICL.0000000000000591. PMID: 30844951.`,
+	`<sup>3</sup>  Davis AR, Sheppard J. Herpes Zoster Ophthalmicus Review and Prevention. Eye Contact Lens. 2019 Sep;45(5):286-291. doi: 10.1097/ICL.0000000000000591. PMID: 30844951.`,
 
-    `<sup>3</sup>  Davis AR, Sheppard J. Herpes Zoster Ophthalmicus Review and Prevention. Eye Contact Lens. 2019 Sep;45(5):286-291. doi: 10.1097/ICL.0000000000000591. PMID: 30844951.`,
+	`<sup>4</sup> Cohen EJ, Jeng BH. Herpes Zoster: A Brief Definitive Review. Cornea. 2021 Aug 1;40(8):943-949. doi: 10.1097/ICO.0000000000002754. PMID: 34029242; PMCID: PMC8249351.`,
 
-    `<sup>4</sup> Cohen EJ, Jeng BH. Herpes Zoster: A Brief Definitive Review. Cornea. 2021 Aug 1;40(8):943-949. doi: 10.1097/ICO.0000000000002754. PMID: 34029242; PMCID: PMC8249351.`,
+	`<sup>5</sup> Vrcek I, Choudhury E, Durairaj V. Herpes Zoster Ophthalmicus: A Review for the Internist. Am J Med. 2017 Jan;130(1):21-26. doi: 10.1016/j.amjmed.2016.08.039. Epub 2016 Sep 17. PMID: 27644149.`,
 
-    `<sup>5</sup> Vrcek I, Choudhury E, Durairaj V. Herpes Zoster Ophthalmicus: A Review for the Internist. Am J Med. 2017 Jan;130(1):21-26. doi: 10.1016/j.amjmed.2016.08.039. Epub 2016 Sep 17. PMID: 27644149.`,
+	`<sup>6</sup> Vrcek I, Choudhury E, Durairaj V. Herpes Zoster Ophthalmicus: A Review for the Internist. Am J Med. 2017 Jan;130(1):21-26. doi: 10.1016/j.amjmed.2016.08.039. Epub 2016 Sep 17. PMID: 27644149.`,
 
-    `<sup>6</sup> Vrcek I, Choudhury E, Durairaj V. Herpes Zoster Ophthalmicus: A Review for the Internist. Am J Med. 2017 Jan;130(1):21-26. doi: 10.1016/j.amjmed.2016.08.039. Epub 2016 Sep 17. PMID: 27644149.`,
+	`<sup>7</sup> Vrcek I, Choudhury E, Durairaj V. Herpes Zoster Ophthalmicus: A Review for the Internist. Am J Med. 2017 Jan;130(1):21-26. doi: 10.1016/j.amjmed.2016.08.039. Epub 2016 Sep 17. PMID: 27644149.`,
 
-    `<sup>7</sup> Vrcek I, Choudhury E, Durairaj V. Herpes Zoster Ophthalmicus: A Review for the Internist. Am J Med. 2017 Jan;130(1):21-26. doi: 10.1016/j.amjmed.2016.08.039. Epub 2016 Sep 17. PMID: 27644149.`,
+	`<sup>8</sup> Davis AR, Sheppard J. Herpes Zoster Ophthalmicus Review and Prevention. Eye Contact Lens. 2019 Sep;45(5):286-291. doi: 10.1097/ICL.0000000000000591. PMID: 30844951.`,
 
-    `<sup>8</sup> Davis AR, Sheppard J. Herpes Zoster Ophthalmicus Review and Prevention. Eye Contact Lens. 2019 Sep;45(5):286-291. doi: 10.1097/ICL.0000000000000591. PMID: 30844951.`,
+	`<sup>9</sup> Davis AR, Sheppard J. Herpes Zoster Ophthalmicus Review and Prevention. Eye Contact Lens. 2019 Sep;45(5):286-291. doi: 10.1097/ICL.0000000000000591. PMID: 30844951.`,
 
-    `<sup>9</sup> Davis AR, Sheppard J. Herpes Zoster Ophthalmicus Review and Prevention. Eye Contact Lens. 2019 Sep;45(5):286-291. doi: 10.1097/ICL.0000000000000591. PMID: 30844951.`,
+	`<sup>10</sup> Li JY. Herpes zoster ophthalmicus: acute keratitis. Curr Opin Ophthalmol. 2018 Jul;29(4):328-333. doi: 10.1097/ICU.0000000000000491. PMID: 29794881.`,
 
-    `<sup>10</sup> Li JY. Herpes zoster ophthalmicus: acute keratitis. Curr Opin Ophthalmol. 2018 Jul;29(4):328-333. doi: 10.1097/ICU.0000000000000491. PMID: 29794881.`,
+	`<sup>11</sup> Thean JH, Hall AJ, Stawell RJ. Uveitis in Herpes zoster ophthalmicus. Clin Exp Ophthalmol. 2001 Dec;29(6):406-10. doi: 10.1046/j.1442-9071.2001.d01-29.x. PMID: 11778812.`,
 
-    `<sup>11</sup> Thean JH, Hall AJ, Stawell RJ. Uveitis in Herpes zoster ophthalmicus. Clin Exp Ophthalmol. 2001 Dec;29(6):406-10. doi: 10.1046/j.1442-9071.2001.d01-29.x. PMID: 11778812.`,
+	`<sup>12</sup> Niederer RL, Meyer JJ, Liu K, Danesh-Meyer HV. Herpes Zoster Ophthalmicus Clinical Presentation and Risk Factors for Loss of Vision. Am J Ophthalmol. 2021 Jun;226:83-89. doi: 10.1016/j.ajo.2021.02.002. Epub 2021 Feb 8. PMID: 33571476.`,
 
-    `<sup>12</sup> Niederer RL, Meyer JJ, Liu K, Danesh-Meyer HV. Herpes Zoster Ophthalmicus Clinical Presentation and Risk Factors for Loss of Vision. Am J Ophthalmol. 2021 Jun;226:83-89. doi: 10.1016/j.ajo.2021.02.002. Epub 2021 Feb 8. PMID: 33571476.`,
+        `<sup>13</sup> Niederer RL, Meyer JJ, Liu K, Danesh-Meyer HV. Herpes Zoster Ophthalmicus Clinical Presentation and Risk Factors for Loss of Vision. Am J Ophthalmol. 2021 Jun;226:83-89. doi: 10.1016/j.ajo.2021.02.002. Epub 2021 Feb 8. PMID: 33571476.`,
 
-    `<sup>13</sup> Niederer RL, Meyer JJ, Liu K, Danesh-Meyer HV. Herpes Zoster Ophthalmicus Clinical Presentation and Risk Factors for Loss of Vision. Am J Ophthalmol. 2021 Jun;226:83-89. doi: 10.1016/j.ajo.2021.02.002. Epub 2021 Feb 8. PMID: 33571476.`,
-  ];
+	
+]
 
-  return (
-    <>
-      <NextSeo
-        title={`${metatitle} | Blog | Hablemos de Zoster`}
-        description={`${metadescription}`}
-        canonical={`${process.env.NEXT_PUBLIC_SITE_URL}/blog/${slug}`}
-        openGraph={{
-          url: `${process.env.NEXT_PUBLIC_SITE_URL}/blog/${slug}`,
-          title: `${metatitle} | Blog |Hablemos de Zoster`,
-          description: `${metadescription}`,
-          images: [
-            {
-              url: `https://gsk-hdz-2.b-cdn.net/og.jpg`,
-              width: 1200,
-              height: 627,
-              alt: "Herpes Zóster",
-              type: "image/jpeg",
-            },
-          ],
-          siteName:
-            "Hablemos de Zóster | Campaña de concientización y prevención",
-        }}
-      />
+	return (
+		<>
+			<NextSeo
+				title={`${metatitle} | Blog | Hablemos de Zoster`}
+				description={`${metadescription}`}
+				canonical={`${process.env.NEXT_PUBLIC_SITE_URL}/blog/${slug}`}
+				openGraph={{
+					url: `${process.env.NEXT_PUBLIC_SITE_URL}/blog/${slug}`,
+					title: `${metatitle} | Blog |Hablemos de Zoster`,
+					description: `${metadescription}`,
+					images: [
+						{
+							url: `https://gsk-hdz-2.b-cdn.net/og.jpg`,
+							width: 1200,
+							height: 627,
+							alt: 'Herpes Zóster',
+							type: 'image/jpeg',
+						}
+					],
+					siteName: 'Hablemos de Zóster | Campaña de concientización y prevención',
+				}}
+			/>
 
-      <>
-        <div className="space-y-8 pb-10">
-          <div className="flex w-full h-96 md:h-[25rem] lg:h-[34rem] xl:h-[42rem] overflow-hidden">
-            <Image
-              src={`https://gsk-hdz-2.b-cdn.net/notas/${image}.webp`}
-              className="w-full h-auto object-cover"
-              alt="Post"
-              width={1920}
-              height={1080}
-              placeholder="blur"
-              blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                shimmer(1920, 1080)
-              )}`}
-              style={{
-                maxWidth: "100%",
-                height: "auto",
-              }}
-            />
-          </div>
+			<>
+				<div className="space-y-8 pb-10">
+					<div className="flex w-full h-96 md:h-[25rem] lg:h-[34rem] xl:h-[42rem] overflow-hidden">
+						<Image src={`https://gsk-hdz-2.b-cdn.net/notas/${image}.webp`}
+							className="w-full h-auto object-cover" alt="Post" width={1920} height={1080}
+							placeholder="blur"
+							blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(1920, 1080))}`}
+							style={{
+								maxWidth: '100%',
+								height: 'auto'
+							}}
+						/>
+					</div>
 
-          <div className="flex flex-col lg:flex-row px-12 gap-x-14">
-            <article className="flex flex-col flex-1 gap-y-4 texto">
-              <header className="space-y-6">
-                {/* <span className="text-xs">01/04/2023 · 8 MINUTOS DE LECTURA</span> */}
+					<div className="flex flex-col lg:flex-row px-12 gap-x-14">
+						<article className="flex flex-col flex-1 gap-y-4 texto">
+							<header className="space-y-6">
+								{/* <span className="text-xs">01/04/2023 · 8 MINUTOS DE LECTURA</span> */}
 
-                <div className="space-y-1">
-                  <DynamicSpeechBlog text={parse(title) + " " + parse(info)} />
+								<div className="space-y-1">
+									<DynamicSpeechBlog text={parse(title) + " " + parse(info)} />
 
-                  <h1 className="text-gsk-orange font-bold leading-none text-4xl sm:text-5xl">
-                    {title}
-                  </h1>
-                  {/* <h2 className="text-gsk-orange text-xl">Ut enim ad minim veniam, quis nostrud exercitation elit.</h2> */}
-                </div>
-              </header>
+									<h1 className="text-gsk-orange font-bold leading-none text-4xl sm:text-5xl">{title}</h1>
+									{/* <h2 className="text-gsk-orange text-xl">Ut enim ad minim veniam, quis nostrud exercitation elit.</h2> */}
+								</div>
+							</header>
 
-              <article>
-                <Markdown
-                  options={{
-                    overrides: {
-                      a: ({ href, children }) => {
-                        return <Link href={href}>{children}</Link>;
-                      },
-                    },
-                  }}
-                  className="markdown-body"
-                >
-                  {info}
-                </Markdown>
-              </article>
+							<article>
+								<Markdown
+									options={{
+										overrides: {
+											a: ({ href, children }) => {
+												return (
+													<Link href={href}>
+														{children}
+													</Link>
+												)
+											},
+										}
+									}}
+									className="markdown-body">{info}</Markdown>
+							</article>
 
-              <CollapsibleList list={list} />
+							<CollapsibleList list={list} />
 
-              {/* <div className="space-y-2 mt-10">
+							{/* <div className="space-y-2 mt-10">
 								<h3 className="text-lg md:text-xl text-gsk-orange">Relacionados</h3>
 								<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
 									{releated.map(({id, title, date}: Post) => (
@@ -282,17 +270,18 @@ const Blog = ({ nota }: any) => {
 									))}
 								</div>
 							</div> */}
-              <div className="space-y-2 mt-2">
-                {/* <p>Cuidá la salud de quienes te rodean:</p> */}
-                {/* <Share cta={`Compartir en`} url={`${process.env.NEXT_PUBLIC_SITE_URL}/blog/${slug}`} quote={title} hashtag={`#${categoria.data.attributes.slug.replaceAll('-', '')}`}/> */}
-              </div>
-            </article>
-          </div>
-        </div>
-        <Banner />
-      </>
-    </>
-  );
-};
+							<div className="space-y-2 mt-2">
+								{/* <p>Cuidá la salud de quienes te rodean:</p> */}
+								{/* <Share cta={`Compartir en`} url={`${process.env.NEXT_PUBLIC_SITE_URL}/blog/${slug}`} quote={title} hashtag={`#${categoria.data.attributes.slug.replaceAll('-', '')}`}/> */}
+							</div>
+						</article>
+						
+					</div>
+				</div>
+				<Banner />
+			</>
+		</>
+	)
+}
 
-export default Blog;
+export default Blog
