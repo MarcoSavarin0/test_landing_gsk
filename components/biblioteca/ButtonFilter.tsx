77d6eec778a6279ba420c12b3759e8727b6974ed
MarcoSavarin0 <ateneo2003@gmail.com>
2024-08-26 17:05:41 -0300

    feat: boton de filtrado para los blogs

--- components/biblioteca/ButtonFilter.tsx
+++ components/biblioteca/ButtonFilter.tsx
@@ -18,17 +18,19 @@ export const ButtonFilter = () => {
 
 	return (
 		<>
-			{list.map((item, index) => (
-				<button
-					key={index}
-					onClick={() => handleClick(index)}
-					className={`${
-						activeIndex === index ? "bg-gsk-orange text-white" : "bg-gsk-gray text-[#959595]"
-					} hover:bg-gsk-orange text-xs hover:text-white rounded-md px-5 py-2 uppercase transition-colors ease-out duration-300 m-2`}
-				>
-					{item}
-				</button>
-			))}
+			<div className="overflow-x-auto whitespace-nowrap md:overscroll-x-none md:whitespace-pre-wrap">
+				{list.map((item, index) => (
+					<button
+						key={index}
+						onClick={() => handleClick(index)}
+						className={`${
+							activeIndex === index ? "bg-gsk-orange text-white" : "bg-gsk-gray text-[#959595]"
+						} hover:bg-gsk-orange text-xs hover:text-white rounded-md px-5 py-2 uppercase transition-colors ease-out duration-300 m-2`}
+					>
+						{item}
+					</button>
+				))}
+			</div>
 		</>
 	)
 }
